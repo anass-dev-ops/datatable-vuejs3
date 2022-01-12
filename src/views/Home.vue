94a380b0ea9e599fa76f7d7ef31ac17f8c906b26
anass-dev-ops <anasszakaria2012@gmail.com>
2022-01-12 15:34:13 +0100

    Upload Project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home mt-5">
+    <h1 class="text-center">Datatable with Vue</h1>
+      <div class="container">
+                
+        <table id="myTable" class="table table-bordered mt-5">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+import $ from "jquery";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  mounted() {
+    this.getPosts()
+  },
+  methods: {
+    getPosts() {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((resp) => {
+        $("#myTable").DataTable({
+          data: resp.data,
+            columns: [
+              { data: "id" },
+              { data: "userId" },
+              { data: "title" },
+              { data: "body" },
+            ],
+        })
+      })
+      .catch((error) => console.log(error.response));
+
+    },
+  },
 }
 </script>
